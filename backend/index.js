0e9eb9ed94df5bde73d40f5456933cb745a046c1
jacky6016 <chlin6016@gmail.com>
2021-07-07 11:13:00 -0700

    Add MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // imported modules
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -10,6 +11,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// connect to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+	console.log('MongoDB database connection established successfully');
+});
 
 // start the server on port
 app.listen(port, () => {
