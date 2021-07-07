93a9d29911abf70872ec18c19e76e78e25f639f4
jacky6016 <chlin6016@gmail.com>
2021-07-07 13:06:16 -0700

    Add initial React components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// self-defined React componenets
+import Navbar from "./components/navbar.component"
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
 function App() {
-  return (
-    <div className="container">
-      Hello world!
-    </div>
-  );
+	return (
+		// routing the usage flow
+		<Router>
+      <div className="container">
+        <Navbar />
+        <br/>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit:id" exact component={EditExercise} />
+        <Route path="/create" exact component={CreateExercise} />
+        <Route path="/user" exact component={CreateUser} />
+      </div>
+		</Router>
+	);
 }
 
 export default App;
